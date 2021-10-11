3ceb16033e697c79a2cc68415935d7a529ae10e9
Sakshi <sakshishetty1231997@gmail.com>
2021-10-11 05:49:47 +0530

    Added: DTO Layer to the employee-payroll app

---
+++ src/main/java/com/bridgelabz/employeepayroll/employeepayrollapp/dto/EmployeeDTO.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeepayroll.employeepayrollapp.dto;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+
+public class EmployeeDTO {
+    private String name;
+    private int salary;
+}
---
+++ src/main/java/com/bridgelabz/employeepayroll/employeepayrollapp/service/EmployeeService.java
@@ -0,0 +1,4 @@
+package com.bridgelabz.employeepayroll.employeepayrollapp.service;
+
+public class EmployeeService {
+}
